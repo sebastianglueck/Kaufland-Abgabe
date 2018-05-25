bfef59e33c900f16ffb5208c3f207b5700209053
Sebastian Glück <sglueck@stud.hs-heilbronn.de>
2018-05-25 13:03:36 +0200

    Initial

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie groß möchten Sie das Feld?");
+            String länge = Console.In.ReadLine();
+            int length = Int32.Parse(länge);
+            Console.WriteLine(length);
+            Map map = new Map(length, length);
+
+
+        }
+    }
+}
